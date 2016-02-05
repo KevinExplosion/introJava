39058844905a3df046c814c558545ffd5b53ea25
kevin <mattisonii@yahoo.com>
2016-02-05 13:27:50 -0800

    Create oneArmedScissor.java, iLoveChocolate.java, codingAwesome.java, and required .class files.

--- Goodbye.java
+++ Goodbye.java
@@ -1,5 +1,28 @@
-public class Goodbye {
-  public static void main(String[] args) {
-    System.out.println("Goodbye World");
+public class Goodbye {  //declare your application's name
+  public static void main(String[] args) {  //method
+    System.out.println("Goodbye World");    //this is where code goes
   }
 }
+
+/*
+public static void main
+public static void main
+public static voic main
+public static void main
+public static void main
+public static void main
+public static void main
+public static void main
+public static void main
+public static void main
+
+public class
+
+public class MyProgramName {
+public static void main([]) {
+
+}
+}
+
+public class TheThing
+public static void main
---
+++ oneArmedScissor.java
@@ -0,0 +1,8 @@
+public class oneArmedScissor {
+  public static void main(String[] args) {
+    System.out.println("Smash transmission");
+    System.out.println("to the one armed scissor");
+    System.out.println("CUT AWAY!!");
+    System.out.println("CUT AWAY!!");
+  }
+}
